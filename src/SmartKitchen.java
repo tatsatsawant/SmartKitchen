8bf51136bba7b237fcbaf8127dff3421aa419393
Tatsat Sawant <tatsatsawant6@gmail.com>
2024-01-18 10:33:21 +0530

    done

---
+++ src/SmartKitchen.java
@@ -0,0 +1,82 @@
+public class SmartKitchen {
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+    private CoffeeMaker brewMaster;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+
+    }
+
+    public void setKitchenState(boolean cofeeFlag, boolean fridgeFlag, boolean dishFlag) {
+        brewMaster.setHasWorkToDo(cofeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishFlag);
+    }
+
+    public void doKitchenWork() {
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+}
+
+class Refrigerator {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood() {
+        if (hasWorkToDo) {
+            System.out.println("Ordering Food!");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class DishWasher {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes() {
+        if (hasWorkToDo) {
+            System.out.println("Washing Dishes!");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+
+class CoffeeMaker {
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee() {
+        if (hasWorkToDo) {
+            System.out.println("Brewing Coffee!");
+            hasWorkToDo = false;
+        }
+    }
+}
